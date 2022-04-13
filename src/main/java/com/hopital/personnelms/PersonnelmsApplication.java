0bc780b9a5083c8a1697b39c9fea52b2a26dbfae
marvinlaisney04 <laisneymarvin@gmail.com>
2022-04-13 16:40:37 +0200

    first commit

---
+++ src/main/java/com/hopital/personnelms/PersonnelmsApplication.java
@@ -0,0 +1,13 @@
+package com.hopital.personnelms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonnelmsApplication.class, args);
+	}
+
+}
