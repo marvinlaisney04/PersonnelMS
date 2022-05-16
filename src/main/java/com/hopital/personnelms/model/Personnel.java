906209ed29cdf1f791b987edfc03789aff92b8a1
LAISNEY <marvin.laisney@sogeti.com>
2022-05-16 15:21:33 +0200

    add Serializable implementation on personnel model

--- src/main/java/com/hopital/personnelms/model/Personnel.java
+++ src/main/java/com/hopital/personnelms/model/Personnel.java
@@ -1,5 +1,7 @@
 package com.hopital.personnelms.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,7 +11,7 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "personnel")
-public class Personnel {
+public class Personnel implements Serializable{
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
